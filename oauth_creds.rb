2931a341baecc8c241d8fffaa0e1f0a9ce21a914
kdhir <kdhir@bu.edu>
2013-10-26 16:06:46 -0400

    initial adding file

---
+++ oauth_creds.rb
@@ -0,0 +1,12 @@
+class MyOauth
+
+	attr_reader :consumer_key, :consumer_secret, :oauth_token, :oauth_token_secret
+	
+	def initialize()
+		@consumer_key =  "VisY26ANpwP8sZIOQN5oQ"
+		@consumer_secret = "HCiWPKeUH5w0hqcIwc1QhZASixwqoAUyI0F6oi8w0"
+		@oauth_token = "480134687-GKbrbHETEWhdLuVzWqVVgCM7USHuWBojdMSZQ4Xu"
+		@oauth_token_secret = "swEtWgUlS0JMSsTAJGT6clcwQEqHxWfZe8mF5rV2XAUyi"
+	end
+	
+end
